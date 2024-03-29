95e0d4a0d0213a7ca182a965f3dfc104136b58c2
vppysk <khdaysxion@gmail.com>
2024-03-29 11:21:57 +0900

    ID指定のGET追加

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,8 +1,22 @@
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import { main } from '../route';
-import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+//プロフィールのID指定取得API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split('/user/')[1]);
+    await main();
+    const user = await prisma.user.findFirst({ where: { id } });
+    return NextResponse.json({ message: 'Success', user }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: 'Error', err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
 //プロフィール編集用API
 export const PUT = async (req: Request, res: NextResponse) => {
