24c80594445bb04d87a3cd8ddd3ca967771ffed3
beppumisato <lacuisinedemisato@gmail.com>
2024-04-04 13:52:21 +0900

    プロフィールフォームのデータ型定義

--- src/app/mypage/edit/page.tsx
+++ src/app/mypage/edit/page.tsx
@@ -3,9 +3,29 @@
 import React from 'react';
 import Link from 'next/link';
 import { useUserState } from '@/app/hooks/user';
+import { useForm } from 'react-hook-form';
+
+// プロフィールフォームのデータ型を定義
+interface ProfileForm {
+  nickname: string;
+  myNationality: string;
+  partnerNationality: string;
+  introduction: string;
+}
 
 export default function EditPage() {
   const { user } = useUserState();
+
+  // useForm関数を呼び出して、各種設定を行う
+  const {
+    register, // inputタグとバリデーションルールを紐付けるための関数
+    handleSubmit, // フォームのsubmitイベント時に呼ばれる関数
+    formState: { errors }, // バリデーションエラーの情報が格納
+  } = useForm<ProfileForm>({ mode: 'onChange' }); // mode: "onChange"で入力時バリデーション
+
+  const onSubmit = async (data: ProfileForm) => {
+    console.log(data);
+  };
 
   return (
     <div className='font-kosugi'>
@@ -19,35 +39,42 @@ export default function EditPage() {
             <div className='border-rose-200 border-2 rounded-full w-14 h-14 mt-5'></div>
           </div>
         </div>
-        <div className='text-rose-400 text-[20px]'>
-          <div className='mt-4 font-bold'>ニックネーム</div>
-          <div className='border-2 border-rose-200 rounded-md p-2 text-black'>
-            {user?.nickname}
-          </div>
-          <div className='mt-6 font-bold'>あなたとお相手の国籍</div>
-          <div className='flex gap-x-2'>
-            <input
-              placeholder='あなたの国籍'
-              type='text'
-              className='border-2 border-rose-200 rounded-md w-1/2 p-2 text-black'
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='text-rose-400 text-[20px]'>
+            <div className='mt-4 font-bold'>ニックネーム</div>
+            <div className='border-2 border-rose-200 rounded-md p-2 text-black'>
+              {user?.nickname}
+            </div>
+            <div className='mt-6 font-bold'>あなたとお相手の国籍</div>
+            <div className='flex gap-x-2'>
+              <input
+                {...register('myNationality')}
+                placeholder='あなたの国籍'
+                type='text'
+                className='border-2 border-rose-200 rounded-md w-1/2 p-2 text-black'
+              />
+              <input
+                {...register('partnerNationality')}
+                placeholder='お相手の国籍'
+                type='text'
+                className='border-2 border-rose-200 rounded-md w-1/2 p-2 text-black'
+              />
+            </div>
+            <div className='mt-6 font-bold'>自己紹介</div>
+            <textarea
+              {...register('introduction')}
+              className='border-2 border-rose-200 rounded-md h-32 w-full p-2 text-black'
             />
-            <input
-              placeholder='お相手の国籍'
-              type='text'
-              className='border-2 border-rose-200 rounded-md w-1/2 p-2 text-black'
-            />
+            <div className='flex justify-center m-5'>
+              <button
+                type='submit'
+                className='bg-rose-400 text-white rounded-md hover:bg-rose-500 w-16 h-6'
+              >
+                保存
+              </button>
+            </div>
           </div>
-          <div className='mt-6 font-bold'>自己紹介</div>
-          <textarea className='border-2 border-rose-200 rounded-md h-32 w-full p-2 text-black' />
-          <div className='flex justify-center m-5'>
-            <button
-              type='submit'
-              className='bg-rose-400 text-white rounded-md hover:bg-rose-500 w-16 h-6'
-            >
-              保存
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
