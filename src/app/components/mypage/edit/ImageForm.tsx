ac58eab827b6ea552a5efc9b4ecc8909133bd2ce
beppumisato <lacuisinedemisato@gmail.com>
2024-04-12 11:34:13 +0900

    関数を定義して置き換え

--- src/app/components/mypage/edit/ImageForm.tsx
+++ src/app/components/mypage/edit/ImageForm.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react';
+import { supabase } from '../../../../../utils/supabase/supabase';
+
+const uploadImage = async (file: File, filePath: string) => {
+  await supabase.storage.from('public-image-bucket').upload(filePath, file);
+
+  //画像の表示
+  const { data } = supabase.storage
+    .from('public-image-bucket')
+    .getPublicUrl(filePath);
+  if (data) {
+    const imageUrl = data.publicUrl;
+
+    console.log(imageUrl);
+  }
+};
 
 export default function ImageForm() {
   // handleChangeFile(e)`の実行
   const [headerImage, setHeaderImage] = useState<File>();
-  const handleChangeHeaderImage = (e: any) => {
+  const handleChangeHeaderImage = async (e: any) => {
     if (e.target.files.length !== 0) {
-      setHeaderImage(e.target.files[0]);
+      const file = e.target.files[0];
+      setHeaderImage(file);
+      const filePath = `header-image/${file.name}`;
+      await uploadImage(file, filePath);
     }
   };
+
   const [iconImage, setIconImage] = useState<File>();
-  const handleChangeIconImage = (e: any) => {
+  const handleChangeIconImage = async (e: any) => {
     if (e.target.files.length !== 0) {
-      setIconImage(e.target.files[0]);
+      const file = e.target.files[0];
+      setIconImage(file);
+      const filePath = `icon-image/${file.name}`;
+      await uploadImage(file, filePath);
     }
   };
 
@@ -20,6 +42,8 @@ export default function ImageForm() {
       <div
         style={{
           backgroundImage: `url(${headerImage ? window.URL.createObjectURL(headerImage) : ''})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
         }}
         className='relative bg-slate-300 w-full h-24 p-2'
       >
