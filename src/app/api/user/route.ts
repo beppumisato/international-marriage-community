932df65813427bec7ed2f614bbf3e4da326f6971
vppysk <khdaysxion@gmail.com>
2024-04-16 14:56:57 +0900

    PUT API追加

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,6 +5,13 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
+const getSub = () => {
+  const headersList = headers();
+  const authorization = headersList.get('authorization');
+  const decoded = jwtDecode(authorization ?? '');
+  return decoded.sub;
+};
+
 export async function main() {
   try {
     await prisma.$connect();
@@ -16,11 +23,7 @@ export async function main() {
 
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    const headersList = headers();
-    const authorization = headersList.get('authorization');
-    const decoded = jwtDecode(authorization ?? '');
-    const sub = decoded.sub;
-
+    const sub = getSub();
     if (!sub) {
       return NextResponse.json(
         { message: 'Invalid authorization' },
@@ -51,6 +54,61 @@ export const GET = async (req: Request, res: NextResponse) => {
         { status: 201 },
       );
     }
+  } catch (err) {
+    return NextResponse.json({ message: 'Error', err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//プロフィール編集用API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const sub = getSub();
+    if (!sub) {
+      return NextResponse.json(
+        { message: 'Invalid authorization' },
+        { status: 401 },
+      );
+    }
+    const {
+      headerImageUrl,
+      iconImageUrl,
+      nickname,
+      myNationality,
+      partnerNationality,
+      introduction,
+    } = await req.json();
+
+    const data = {
+      headerImageUrl: headerImageUrl,
+      iconImageUrl: iconImageUrl,
+      nickname: nickname,
+      myNationality: myNationality,
+      partnerNationality: partnerNationality,
+      introduction: introduction,
+    };
+    for (let k in data) {
+      if (data[k] === '') {
+        delete data[k];
+      }
+    }
+    await main();
+
+    const currentUser = await prisma.user.findFirst({
+      where: { cognitoSub: sub },
+    });
+
+    if (!currentUser) {
+      return NextResponse.json({ message: 'Not Found' }, { status: 404 });
+    }
+
+    const user = await prisma.user.update({
+      data: data,
+      where: { id: currentUser.id },
+    });
+
+    return NextResponse.json({ message: 'Success', user }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: 'Error', err }, { status: 500 });
   } finally {
