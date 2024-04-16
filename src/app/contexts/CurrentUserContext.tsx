929f96ea22cecd290dc8cd8376f6109b75d93950
vppysk <khdaysxion@gmail.com>
2024-04-16 13:41:47 +0900

    ログイン状態に応じた処理追加

--- src/app/contexts/CurrentUserContext.tsx
+++ src/app/contexts/CurrentUserContext.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { User } from '@prisma/client';
+import { usePathname, useRouter } from 'next/navigation';
 import {
   Dispatch,
   ReactNode,
   SetStateAction,
   createContext,
+  useEffect,
   useState,
 } from 'react';
+import { Cognito } from '../../../utils/cognito';
+import { fetchUserData } from '../repositories/user';
 
 export const CurrentUserContext = createContext<{
   currentUser: User | null;
@@ -22,7 +26,45 @@ type Props = Required<{
 }>;
 
 export default function CurrentUserProvider(props: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const cognito = new Cognito();
+    cognito.getSession().then((session) => {
+      // 未ログインの場合
+      if (!session) {
+        // TOP、ログイン、新規登録ページ以外はログインページへ
+        if (
+          ![
+            '/',
+            '/login',
+            '/registration',
+            '/registration/confirmation',
+          ].includes(pathname)
+        ) {
+          router.push('/login');
+        }
+      } else {
+        // ログイン済みでログイン、新規登録ページを開いた場合はマイページへ
+        if (
+          ['/login', '/registration', '/registration/confirmation'].includes(
+            pathname,
+          )
+        ) {
+          return router.push('/mypage');
+        }
+
+        // ログイン済みでユーザーがない場合は取得する
+        if (!user) {
+          fetchUserData().then((user) => {
+            if (user) setUser(user);
+          });
+        }
+      }
+    });
+  }, [pathname]);
 
   return (
     <CurrentUserContext.Provider
