7ff4e5d69586d850426c24b33b2844dce5802246
beppumisato <lacuisinedemisato@gmail.com>
2024-04-22 13:33:38 +0900

    デザイン作成

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -3,22 +3,29 @@ import { SidebarData } from './SidebarData';
 
 export default function Sidebar() {
   return (
-    <div className='Sidebar'>
-      <ul className='SidebarList'>
-        {SidebarData.map((value, key) => {
-          return (
-            <li
-              key={key}
-              // id={(window.location.pathname = value.link ? 'active' : '')}
-              className='row'
-              onClick={() => (window.location.pathname = value.link)}
-            >
-              <div id='icon'>{value.icon}</div>
-              <div id='icon'>{value.title}</div>
-            </li>
-          );
-        })}
-      </ul>
+    <div className='absolute'>
+      <div className='w-24 h-screen bg-rose-300 text-white text-[20px]'>
+        <h1 className='text-[28px] flex justify-center border-b-2 text-white'>
+          MENU
+        </h1>
+        <ul className='h-auto w-full p-0 pt-4'>
+          {SidebarData.map((value, key) => {
+            return (
+              <li
+                key={key}
+                // id={(window.location.pathname = value.link ? 'active' : '')}
+                className='flex w-100 h-6 hover:bg-rose-500 cursor-pointer justify-start items-center'
+                onClick={() => (window.location.pathname = value.link)}
+              >
+                <div id='id' className='mr-2 ml-2'>
+                  {value.icon}
+                </div>
+                <div id='icon'>{value.title}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
