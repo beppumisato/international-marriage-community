da2baee31d36c89644c4ddc8127573d4254d3968
beppumisato <lacuisinedemisato@gmail.com>
2024-04-23 10:47:54 +0900

    画面遷移した時のサイドバーを閉じる動作実装

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -4,16 +4,23 @@ import Link from 'next/link';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { sidebarDataLogin, sidebarDataNotLogin } from './sidebarData';
 import { CurrentUserContext } from '@/app/contexts/CurrentUserContext';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   onClick: () => void;
 }
 
 export default function Sidebar(props: Props) {
+  const router = useRouter();
   const { user } = useContext(CurrentUserContext);
 
   const isLogin = user != null;
   const sidebarData = isLogin ? sidebarDataLogin : sidebarDataNotLogin;
+
+  const onClickLink = (link: string) => {
+    router.push(link);
+    props.onClick();
+  };
 
   return (
     <div className='absolute top-0 right-0 z-50'>
@@ -28,16 +35,16 @@ export default function Sidebar(props: Props) {
         <ul className='h-auto w-full p-0 pt-4'>
           {sidebarData.map((value, key) => {
             return (
-              <Link
-                href={value.link}
+              <button
+                onClick={() => onClickLink(value.link)}
                 key={key}
-                className='flex w-100 h-6 hover:bg-rose-500 cursor-pointer justify-start items-center'
+                className='flex w-full h-6 hover:bg-rose-500 cursor-pointer justify-start items-center'
               >
                 <div className='mr-2 ml-2'>
                   <ArrowBackIosNewIcon sx={{ fontSize: 20 }} />
                 </div>
                 <div>{value.title}</div>
-              </Link>
+              </button>
             );
           })}
         </ul>
