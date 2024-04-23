03cc62f08210e155ac8644b7ac8e9a0c11b954eb
beppumisato <lacuisinedemisato@gmail.com>
2024-04-23 09:51:48 +0900

    サイドバー表示位置の修正

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import MenuIcon from '@mui/icons-material/Menu';
+import Sidebar from '../sidebar/Sidebar';
 
 interface Props {
   title: string;
@@ -8,11 +9,15 @@ interface Props {
 
 export default function Header() {
   return (
-    <div className='bg-rose-200 pl-2 h-6 flex justify-between items-center px-2'>
-      <Link href={`/`}>
-        <h1 className='text-white'>タイトル</h1>
-      </Link>
-      <MenuIcon sx={{ fontSize: 40, color: 'white' }} />
+    <div className='relative'>
+      <Sidebar />
+
+      <div className='bg-rose-200 pl-2 h-6 flex justify-between items-center px-2'>
+        <Link href={`/`}>
+          <h1 className='text-white'>タイトル</h1>
+        </Link>
+        <MenuIcon sx={{ fontSize: 40, color: 'white' }} />
+      </div>
     </div>
   );
 }
--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { SidebarData } from './SidebarData';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Sidebar() {
   return (
-    <div className='absolute'>
+    <div className='absolute top-0 right-0 z-50'>
       <div className='w-24 h-screen bg-rose-300 text-white text-[20px]'>
-        <button className='size-[32px] flex justify-end'>✕</button>
+        <CloseIcon sx={{ fontSize: 40, color: 'white' }} />
         <h1 className='text-[28px] border-b-2 flex justify-center text-white'>
           MENU
         </h1>
@@ -16,7 +17,6 @@ export default function Sidebar() {
                 key={key}
                 // id={(window.location.pathname = value.link ? 'active' : '')}
                 className='flex w-100 h-6 hover:bg-rose-500 cursor-pointer justify-start items-center'
-                onClick={() => (window.location.pathname = value.link)}
               >
                 <div id='id' className='mr-2 ml-2'>
                   {value.icon}
