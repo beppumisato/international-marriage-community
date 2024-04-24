e2604fb47c059fe8d3a97b38cc768488e06b0178
beppumisato <lacuisinedemisato@gmail.com>
2024-04-24 16:45:24 +0900

    Timelineのコンポーネントを分けてデザイン変更

---
+++ src/app/components/timeline/blog/BlogDisplay.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { Post, User } from '@prisma/client';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+async function fetchAllBlogs() {
+  const res = await fetch(`http://localhost:3000/api/blog`, {
+    cache: 'no-store', //SSR
+  });
+
+  const data = await res.json();
+  return data.posts;
+}
+
+type Blog = Post & { author: User };
+
+export default function BlogDisplayPage() {
+  const [posts, setPosts] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    fetchAllBlogs().then((posts) => {
+      setPosts(posts);
+    });
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className='bg-white p-6 ml-2 w-4/5'>
+        <form onSubmit={handleSubmit}>
+          {posts.map((post) => (
+            <div key={post.id}>
+              <div className='p-1 m-4 mb-0'>
+                <div className='flex'>
+                  <img
+                    width={50}
+                    className='rounded-full max-w-full h-auto align-middle border-2 border-yellow-700'
+                    src={post.author.iconImageUrl}
+                  ></img>
+                  <div className='absolute text-[18px] mt-6'>
+                    {post.author.nickname}
+                  </div>
+
+                  <div className='absolute left-56'>
+                    <div className='text-[14px] text-slate-500'>
+                      {new Date(post.createdAt).toDateString()}
+                    </div>
+                    <h1 className='text-[20px]'>{post.title}</h1>
+                    <h2 className='text-[18px]'>{post.description}</h2>
+                  </div>
+                </div>
+                <div className='flex justify-end'>
+                  <Link
+                    href={`/blog/edit/${post.id}`}
+                    className='text-yellow-700 text-[20px]'
+                  >
+                    ...
+                  </Link>
+                </div>
+              </div>
+              <div className='border-b border-yellow-700'></div>
+            </div>
+          ))}
+        </form>
+      </div>
+    </>
+  );
+}
--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,80 +1,27 @@
 'use client';
 
-import Header from '@/app/components/common/Header';
 import { Post, User } from '@prisma/client';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
+import BlogDisplayPage from '../components/timeline/blog/BlogDisplay';
+import ProfileDisplay from '../components/mypage/ProfileDisplay';
+import ProfileDisplayPage from '../components/timeline/profile/ProgileDisplay';
 
-async function fetchAllBlogs() {
-  const res = await fetch(`http://localhost:3000/api/blog`, {
-    cache: 'no-store', //SSR
-  });
-
-  const data = await res.json();
-  return data.posts;
-}
-
-type Blog = Post & { author: User };
-
-export default function AllBlogPage() {
-  const [posts, setPosts] = useState<Blog[]>([]);
-
-  useEffect(() => {
-    fetchAllBlogs().then((posts) => {
-      setPosts(posts);
-    });
-  }, []);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
-  const returnTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
+export default function TimelinePage() {
+  // // Topへスクロー機能
+  // const returnTop = () => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth',
+  //   });
+  // };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className='font-kosugi'>
-          {posts.map((post) => (
-            <div key={post.id}>
-              <div>
-                <div className='flex'>
-                  <img
-                    width={50}
-                    className='rounded-full max-w-full h-auto align-middle border-none'
-                    src={post.author.iconImageUrl}
-                  ></img>
-                  <div className='absolute left-24'>
-                    <div className='text-[14px] text-slate-500'>
-                      {new Date(post.createdAt).toDateString()}
-                    </div>
-                    <h1 className='text-[20px]'>{post.title}</h1>
-                    <h2 className='text-[18px]'>{post.description}</h2>
-                  </div>
-                </div>
-                <div className='absolute left-6 text-[18px]'>
-                  {post.author.nickname}
-                </div>
-                <div className='flex justify-end'>
-                  <Link
-                    href={`/blog/edit/${post.id}`}
-                    className='text-rose-300 text-[24px]'
-                  >
-                    ...
-                  </Link>
-                </div>
-              </div>
-              <div className='border-b border-rose-200'></div>
-            </div>
-          ))}
-          {/* <img src='/icon/airplan_2.png' onClick={returnTop}></img> */}
-        </div>
-      </form>
+      <div className='font-kosugi p-4 flex'>
+        <ProfileDisplayPage />
+        <BlogDisplayPage />
+      </div>
     </>
   );
 }
