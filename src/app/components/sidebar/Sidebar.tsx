46ead1ee0bed359351b5f20b3b06d13d95d56778
beppumisato <lacuisinedemisato@gmail.com>
2024-04-27 16:13:41 +0900

    sideBar Animation実装

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { sidebarDataLogin, sidebarDataNotLogin } from './sidebarData';
 import { CurrentUserContext } from '@/app/contexts/CurrentUserContext';
 import { useRouter } from 'next/navigation';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Props {
   onClick: () => void;
@@ -22,31 +23,49 @@ export default function Sidebar(props: Props) {
   };
 
   return (
-    <div className='absolute top-0 right-0 z-50'>
-      <div className='w-24 h-screen bg-orange-300 text-white text-[20px]'>
-        <div className='flex justify-end p-2'>
-          <CloseIcon
-            onClick={props.onClick}
-            sx={{ fontSize: 40, color: 'white' }}
-          />
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        exit={{
+          opacity: 0,
+          x: 0,
+          width: 0,
+          minWidth: 0,
+          transition: { opacity: { duration: 0.3 }, duration: 0.3 },
+        }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className='absolute top-0 right-0 z-50'>
+          <div className='w-24 h-screen bg-orange-300 text-white text-[20px]'>
+            <div className='flex justify-end p-2'>
+              <CloseIcon
+                onClick={props.onClick}
+                sx={{ fontSize: 40, color: 'white' }}
+              />
+            </div>
+            <ul className='h-auto w-full p-0 pt-4'>
+              {sidebarData.map((value, key) => {
+                return (
+                  <button
+                    onClick={() => onClickLink(value.link)}
+                    key={key}
+                    className='flex w-full h-6 hover:bg-orange-400 cursor-pointer justify-start items-center'
+                  >
+                    <div className='mr-2 ml-2'>
+                      <ArrowBackIosNewIcon sx={{ fontSize: 20 }} />
+                    </div>
+                    <div>{value.title}</div>
+                  </button>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <ul className='h-auto w-full p-0 pt-4'>
-          {sidebarData.map((value, key) => {
-            return (
-              <button
-                onClick={() => onClickLink(value.link)}
-                key={key}
-                className='flex w-full h-6 hover:bg-orange-400 cursor-pointer justify-start items-center'
-              >
-                <div className='mr-2 ml-2'>
-                  <ArrowBackIosNewIcon sx={{ fontSize: 20 }} />
-                </div>
-                <div>{value.title}</div>
-              </button>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
