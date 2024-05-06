3f7b115da0cfe66f3ae1943c38934ce5099628ae
beppumisato <lacuisinedemisato@gmail.com>
2024-05-06 18:57:03 +0900

    mypageの修正

--- src/app/components/mypage/ProfileDisplay.tsx
+++ src/app/components/mypage/ProfileDisplay.tsx
@@ -3,48 +3,45 @@
 import { useContext } from 'react';
 import Link from 'next/link';
 import { CurrentUserContext } from '@/app/contexts/CurrentUserContext';
+import PublicIcon from '@mui/icons-material/Public';
 
 export default function ProfileDisplay() {
   const { user } = useContext(CurrentUserContext);
 
   return (
-    <div className='font-kosugi p-4'>
-      <img
-        className='relative w-full h-24 border-2 border-yellow-700'
-        src={user?.headerImageUrl}
-      ></img>
-      <img
-        className='absolute rounded-full max-w-full h-auto align-middle border-2 border-yellow-700 top-28 left-6'
-        width={100}
-        src={user?.iconImageUrl}
-      ></img>
-      <div className='flex justify-between mt-2'>
-        <div className='text-[34px] text-yellow-700 mt-2 ml-2'>
-          {user?.nickname}
-        </div>
-        <Link
-          href={`/mypage/edit/`}
-          className='w-20 h-5 bg-orange-400 rounded-md text-white text-[20px] flex items-center justify-center'
-        >
-          プロフィール編集
-        </Link>
+    <div className='font-kosugi p-10'>
+      <div>
+        <img className='relative w-full h-40' src={user?.headerImageUrl}></img>
+        <img
+          className='absolute rounded-full max-w-full h-auto align-middle shadow shadow-slate-400 top-40 m-4'
+          width={100}
+          src={user?.iconImageUrl}
+        ></img>
       </div>
-      <div className='flex mt-1'>
-        <div className='border-yellow-700 border-2 h-5 w-16 rounded-md text-[18px] text-center p-1'>
-          <div className='flex'>
-            <img src='/icon/pink.png' className='mr-2' width={30}></img>
-            {user?.myNationality}
-          </div>
+
+      <div className='p-4 border-b-2'>
+        <div className='flex justify-between'>
+          <div className='text-[24px] ml-3 mt-6'>{user?.nickname}</div>
+          <Link
+            href={`/mypage/edit/`}
+            className='w-36 h-10 bg-orange-400 rounded hover:bg-orange-500 text-white text-[14px] flex items-center justify-center'
+          >
+            プロフィール編集
+          </Link>
         </div>
-        <div className='border-yellow-700 border-2 h-5 w-16 rounded-md text-[18px] text-center p-1 ml-1'>
-          <div className='flex'>
-            <img src='/icon/blue.png' className='mr-2' width={30}></img>
-            {user?.partnerNationality}
+        <div className='ml-3 my-2'>
+          <div className='flex gap-x-4 text-[20px] mb-2'>
+            <div>
+              <PublicIcon sx={{ fontSize: 20, color: '#f472b6' }} />
+              {user?.myNationality}
+            </div>
+            <div>
+              <PublicIcon sx={{ fontSize: 20, color: '#38bdf8' }} />
+              {user?.partnerNationality}
+            </div>
           </div>
+          {user?.introduction}
         </div>
-      </div>
-      <div className='text-[18px] border-yellow-700 border-2 rounded-md h-auto w-auto mt-1 p-2'>
-        {user?.introduction}
       </div>
     </div>
   );
