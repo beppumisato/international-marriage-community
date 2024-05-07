e46506fbea246c62208736b74026606e39f1b10d
beppumisato <lacuisinedemisato@gmail.com>
2024-05-07 20:22:33 +0900

    画面スクロールボタンの実装

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,15 +3,19 @@
 import React from 'react';
 import ProfileDisplay from '../components/mypage/ProfileDisplay';
 import TimelineBlogDisplay from '../components/timeline/blog/BlogDisplay';
+import ScrollTop from '../components/scroll/ScrollTop';
 
 export default function MyPage() {
   return (
-    <div className='font-kosugi'>
-      <ProfileDisplay />
+    <>
+      <div className='font-kosugi'>
+        <ProfileDisplay />
 
-      <div className='flex justify-center'>
-        <TimelineBlogDisplay isMyPost={true} />
+        <div className='flex justify-center'>
+          <TimelineBlogDisplay isMyPost={true} />
+        </div>
       </div>
-    </div>
+      <ScrollTop />
+    </>
   );
 }
