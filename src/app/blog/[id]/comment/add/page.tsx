a61c892fcf1180fb740e6bd3eaece0c4d269adbf
beppumisato <lacuisinedemisato@gmail.com>
2024-06-07 15:41:07 +0900

    コメントページの作成

--- src/app/blog/[id]/comment/add/page.tsx
+++ src/app/blog/[id]/comment/add/page.tsx
@@ -1,83 +1,68 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Modal from '@/app/components/modal/Modal';
 import Link from 'next/link';
-import { deleteBlog, editBlog, getBlogById } from '@/app/repositories/blog';
+import { Post, User } from '@prisma/client';
+import { fetchAllBlogs } from '@/app/repositories/blog/fetchAllBlogs';
+import { useRouter } from 'next/navigation';
 
-export default function CommentAddtPage({
-  params,
-}: {
-  params: { id: number };
-}) {
-  const titleRef = useRef<HTMLInputElement | null>(null);
+type Blog = Post & { author: User };
+
+type Props = {
+  isMyPost: boolean;
+};
+
+export default function TimelineBlogDisplay(props: Props) {
+  const [posts, setPosts] = useState<Blog[]>([]);
   const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
 
+  useEffect(() => {
+    fetchAllBlogs(props.isMyPost).then((posts) => {
+      setPosts(posts);
+    });
+  }, []);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    await editBlog(
-      titleRef.current?.value,
-      descriptionRef.current?.value,
-      params.id,
-    );
   };
-
-  const handleDelete = async () => {
-    await deleteBlog(params.id);
-  };
-
-  useEffect(() => {
-    getBlogById(params.id)
-      .then((data) => {
-        console.log(data);
-        if (titleRef.current && descriptionRef.current) {
-          titleRef.current.value = data.title;
-          descriptionRef.current.value = data.description;
-        }
-      })
-      .catch((err) => {});
-  }, []);
 
   const router = useRouter();
 
   return (
     <div className='font-kosugi px-32'>
       <form onSubmit={handleSubmit}>
-        <input
-          ref={titleRef}
-          type='text'
-          placeholder='記事タイトル'
-          className='w-full h-10 border-2 text-[20px] p-2 rounded mt-10'
-        />
-        <textarea
-          ref={descriptionRef}
-          placeholder='国際結婚にまつわる問題や悩みにおいて、あなたの経験から役立つと思う情報を書いて共有しよう。'
-          className='w-full h-80 border-2 text-[18px] p-2 rounded mt-3'
-        />
-        <textarea
-          ref={descriptionRef}
-          placeholder='疑問に思ったことがあれば質問して聞いてみよう。'
-          className='w-full h-80 border-2 border-rose-400 text-[18px] p-2 rounded mt-3'
-        />
+        {posts.map((post) => (
+          <div key={post.id}>
+            <div className='w-full h-10 border-2 text-[20px] p-2 rounded mt-10'>
+              {post.title}
+            </div>
+            <div className='w-full h-80 border-2 text-[18px] p-2 rounded mt-3'>
+              {post.description}
+            </div>
+            <textarea
+              ref={descriptionRef}
+              placeholder='疑問に思ったことがあれば質問して聞いてみよう。'
+              className='w-full h-80 border-2 border-rose-400 text-[18px] p-2 rounded mt-3'
+            />
 
-        <div className='flex justify-center gap-10 mt-28'>
-          <Link href={'/timeline/'}>
-            <button className='buttonC hover:bg-sky-300'>キャンセル</button>
-          </Link>
-          <button
-            onClick={() => {
-              setModalOpen(true);
-              router.back();
-            }}
-            type='submit'
-            className='button hover:bg-orange-200'
-          >
-            変更を保存
-          </button>
-        </div>
+            <div className='flex justify-center gap-10 mt-28'>
+              <Link href={'/timeline/'}>
+                <button className='buttonC hover:bg-sky-300'>キャンセル</button>
+              </Link>
+              <button
+                onClick={() => {
+                  setModalOpen(true);
+                  router.back();
+                }}
+                type='submit'
+                className='button hover:bg-orange-200'
+              >
+                変更を保存
+              </button>
+            </div>
+          </div>
+        ))}
       </form>
     </div>
   );
