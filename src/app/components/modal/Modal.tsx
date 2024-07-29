8d34823d72cad3158e2ea4cd2bc08f15181f81c1
beppumisato <lacuisinedemisato@gmail.com>
2024-07-29 11:52:07 +0900

    ポップアップ時の操作修正

--- src/app/components/modal/Modal.tsx
+++ src/app/components/modal/Modal.tsx
@@ -9,40 +9,44 @@ interface Props {
 
 export default function Modal(props: Props) {
   return (
-    <div className='fixed h-60 w-96 top-60'>
-      <div className='p-2 h-60 w-96 bg-white shadow shadow-slate-400 text-[16px] flex-col'>
-        <div className='flex justify-end'>
-          <button
-            onClick={() => {
-              props.setOpenModal(false);
-            }}
-          >
-            <CloseIcon sx={{ fontSize: 30 }} />
-          </button>
-        </div>
-        <h1 className='text-center mt-10'>この投稿を本当に削除しますか？</h1>
-        <div className='flex gap-4 justify-center mt-10'>
-          <button
-            onClick={() => {
-              props.setOpenModal(false);
-            }}
-            className='text-slate-500 border-2 border-slate-500 rounded hover:bg-sky-300 w-32 h-8'
-          >
-            キャンセル
-          </button>
-          <Link href={'/timeline/'}>
+    <>
+      {/* ポップアップ表示中の背景色とほか操作の制限 */}
+      <div className='z-40 w-full h-full fixed inset-0 bg-black opacity-50'></div>
+      <div className='z-50 w-full h-full fixed inset-0 flex items-center justify-center'>
+        <div className='w-3/4 lg:w-1/2 bg-white rounded-xl py-12'>
+          <div className='flex justify-end'>
             <button
               onClick={() => {
-                props.onYes();
-                props.setOpenModal(true);
+                props.setOpenModal(false);
               }}
-              className='text-white bg-red-500 rounded hover:bg-yellow-300 w-32 h-8'
             >
-              削除
+              <CloseIcon sx={{ fontSize: 30 }} />
             </button>
-          </Link>
+          </div>
+          <h1 className='text-center mt-10'>この投稿を本当に削除しますか？</h1>
+          <div className='flex gap-4 justify-center mt-10'>
+            <button
+              onClick={() => {
+                props.setOpenModal(false);
+              }}
+              className='text-slate-500 border-2 border-slate-500 rounded hover:bg-sky-300 w-32 h-8'
+            >
+              キャンセル
+            </button>
+            <Link href={'/timeline/'}>
+              <button
+                onClick={() => {
+                  props.onYes();
+                  props.setOpenModal(true);
+                }}
+                className='text-white bg-red-500 rounded hover:bg-yellow-300 w-32 h-8'
+              >
+                削除
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
